c2ae05059fd235077982797b585b89b51ed13963
Sm <Dragondragon.wu21@gmail.com>
2025-01-30 09:51:57 -0800

    Torso Movement Complete

--- Chronos_X_Machina/Assets/Scripts/Shane/PlayerController.cs
+++ Chronos_X_Machina/Assets/Scripts/Shane/PlayerController.cs
@@ -1,62 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Playables;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
 {
-    GameObject Legs;
-    GameObject UpperTorso;
-    Vector3 mousePos;
+    private CharacterController characterController;
+    
+    public GameObject Legs;
+    public GameObject UpperTorso;
+    public Vector3 mousePos;
 
+    public GameObject tester;
 
+    public float rotationSpeed = 100;
+
+    private float inActionDelay = .0000000000000000000000000000000000000001f;
+    public int frameRate = 30;
+
+    private bool available = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }   
 
 
     void FixedUpdate()
     {
-        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-        Debug.Log(mousePos.ToString());
-
-        //UpperTorso.transform.rotation.y = Lerp(mousePos, 0, 1);
-    }
-
-
-    private IEnumerator TorsoRotate()
-    {
-        
-        yield return new WaitForSeconds(0.5f);
-
-        float t = 0;
-        int frames = 0;
+        //
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(movement * Time.deltaTime);
+        //
 
 
-        // Step 2: Open the locker doors
-        while (Quaternion.Angle(UpperTorso.transform.localRotation, facingMouseRotation) > 0.1f)
+        //Torso Rotation: Raycast to find pos of mouse, then calc dist from mouse to torso, clamps x , z rotation making only y rotation.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
+            //Debug.Log(hit.transform.name);
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log("hit: " + hit.point);
 
-            t += Time.deltaTime;
-            UpperTorso.transform.localRotation = Quaternion.Lerp(UpperTorso.transform.localRotation, leftOpenRotation, t);
-
-            frames++;
-            if (frames >= frameRate)
-            {
-                yield return new WaitForSeconds(inActionDelay + 0.03f);
-                frames = 0;
-            }
+            tester.transform.position = hit.point;
         }
-        t = 0;
-        //Vector3 targetPosition = BeforeHiding;
+
+        var lookPos = hit.point - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        UpperTorso.transform.rotation = Quaternion.Slerp(UpperTorso.transform.rotation, rotation, Time.deltaTime * 30);
+        //END OF TORSO ROTATION
+
+
+
+
     }
 }
