30ab08867806a969d68ce55f1b953cd70c709cad
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2025-03-05 20:45:13 -0800

    asd

---
+++ Chronos_X_Machina/Assets/Scripts/Shane/PlayerController_Elizeo.cs
@@ -0,0 +1,215 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerController_Elizeo : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public GameObject Tester;
+    public GameObject Legs;
+    public GameObject UpperTorso;
+    public GameObject FlarePrefab;
+    public Transform LeftFlareSpawnPoint;
+    public Transform RightFlareSpawnPoint;
+    public int maxFlareCharges = 3;
+    private int currentFlareCharges;
+
+    //private float deathTime = 1f;
+    
+    //This is where Elizeo's Codes are located.
+    [Header("---------------------------------------------------------------------------------------------------------------------")]
+
+    [Header("Player HP - Added by Elizeo:")]
+    public int maxHP;
+    public static int currentHP;
+
+    [Header("This is where Elizeo's HP bar will be located.")]
+    public PlayerHP playerHPUI;
+
+    [Header("Where will the player spawn?")]
+    public Transform playerSpawn;
+
+    [Header("How many lives does the player have?")]
+    public int playerLives;
+    [SerializeField]private int currentLives;
+
+    [Header("How much HP can the player heal?")]
+    public int healValue;
+
+    [Header("These are the Player Cameras")]
+    public GameObject waveCam;
+    public GameObject playerCam;
+
+    [Header("---------------------------------------------------------------------------------------------------------------------")]
+
+    public float flareSpeed = 10f;
+    public float speed;
+    public float rotationSpeed = 100;
+    public int flaresPerShot = 5;
+    public float flareSpreadAngle = 20f;
+    public float flareArcAngle = 30f;
+    public float verticalArcAngle = 15f;
+    public float randomVelocityFactor = 3f;
+    public float upwardBoost = 5f;
+
+    void Start()
+    {
+        currentHP = maxHP;
+        currentLives = playerLives;
+        characterController = GetComponent<CharacterController>();
+        currentFlareCharges = maxFlareCharges;
+
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (move.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            Legs.transform.rotation = Quaternion.Slerp(Legs.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Q) && currentFlareCharges > 0)
+        {
+            ShootFlares();
+        }
+
+        if (!WaveChecker.insideWave)
+        {
+            waveCam.SetActive(false);
+            playerCam.SetActive(true);
+        }
+        else
+        {
+            waveCam.SetActive(true);
+            playerCam.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Set up the ray from the mouse position to the scene
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Create a LayerMask that will ignore both the "Player" and "UI" layers
+        int layerMask = LayerMask.GetMask("Player", "UI");  // Get mask for both layers
+
+        // Cast the ray, but ignore the "Player" and "UI" layers
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~layerMask)) // The tilde (~) inverts the mask to ignore those layers
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Tester.transform.position = hit.point;
+        }
+        else
+        {                   
+            // In case no hit occurs, move the object far enough along the ray
+            Tester.transform.position = ray.GetPoint(1000); // Move it in the direction of the ray, no obstacle
+        }
+
+        // Calculate and rotate the torso to face the hit point
+        var lookPos = hit.point - transform.position;
+        lookPos.y = 0; // Lock rotation to the Y-axis
+        var rotation = Quaternion.LookRotation(lookPos);
+        UpperTorso.transform.rotation = Quaternion.Slerp(UpperTorso.transform.rotation, rotation, Time.deltaTime * 30);
+
+
+        if (currentHP <= 0)
+        {
+            Respawn();
+            if (currentLives <= -1)
+            {
+                currentHP = 0;
+                currentLives = 0;
+                GameOver.isGameOver = true;
+            }
+        }
+    }
+
+    void ShootFlares()
+    {
+        if (FlarePrefab != null && LeftFlareSpawnPoint != null && RightFlareSpawnPoint != null)
+        {
+            for (int i = 0; i < flaresPerShot; i++)
+            {
+                float horizontalOffset = Random.Range(-flareSpreadAngle, flareSpreadAngle);
+                float verticalOffset = Random.Range(-verticalArcAngle, verticalArcAngle);
+
+                Quaternion leftFlareRotation = Quaternion.Euler(verticalOffset, -flareArcAngle + horizontalOffset, 0) * transform.rotation;
+                Quaternion rightFlareRotation = Quaternion.Euler(verticalOffset, flareArcAngle + horizontalOffset, 0) * transform.rotation;
+
+                SpawnFlare(LeftFlareSpawnPoint, leftFlareRotation, -transform.right);
+                SpawnFlare(RightFlareSpawnPoint, rightFlareRotation, transform.right);
+            }
+            currentFlareCharges--;
+        }
+    }
+
+    void SpawnFlare(Transform spawnPoint, Quaternion rotation, Vector3 direction)
+    {
+        GameObject flare = Instantiate(FlarePrefab, spawnPoint.position, rotation);
+        Rigidbody rb = flare.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 randomOffset = new Vector3(
+                Random.Range(-randomVelocityFactor, randomVelocityFactor),
+                Random.Range(0, randomVelocityFactor) + upwardBoost,
+                Random.Range(-randomVelocityFactor, randomVelocityFactor)
+            );
+            rb.velocity = (direction * flareSpeed) + randomOffset;
+        }
+    }
+
+    void Respawn()
+    {
+        currentLives -= 1;
+        currentHP = maxHP;
+        transform.position = playerSpawn.transform.position;
+        playerHPUI.SetHP(maxHP);
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyWep"))
+        {
+            currentHP -= 25;
+            playerHPUI.SetHP(currentHP);
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Explosion"))
+        {
+            currentHP -= 25;
+            playerHPUI.SetHP(currentHP);
+            other.gameObject.GetComponent<Collider>().enabled = false;
+        }
+
+        if (other.gameObject.CompareTag("EnemyBomb"))
+        {
+            currentHP -= 25;
+            playerHPUI.SetHP(currentHP);
+            other.gameObject.GetComponent<Collider>().enabled = false;
+        }
+
+        if (other.gameObject.CompareTag("Kamikaze"))
+        {
+            currentHP -= 50;
+            playerHPUI.SetHP(currentHP);
+        }
+
+        if (other.gameObject.CompareTag("Heal"))
+        {
+            currentHP += healValue;
+            playerHPUI.SetHP(currentHP);
+            Destroy(other.gameObject);
+        }
+    }
+}
