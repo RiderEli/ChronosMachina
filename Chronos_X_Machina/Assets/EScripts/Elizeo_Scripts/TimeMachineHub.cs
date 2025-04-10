15a7ce5a2417f19b215bbbc804253980fcb97502
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2025-04-09 19:50:36 -0700

    TIME MACHINE MIDDLE UWU

--- Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/TimeMachineHub.cs
+++ Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/TimeMachineHub.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 /* [Nava, Elizeo]
@@ -10,14 +11,38 @@ public class TimeMachineHub : MonoBehaviour
 {
     public GameObject mainHub;
     public GameObject sceneHub;
+    public GameObject middleHub;
     //Artifact Hub
     //Upgrade Hub
 
+    public bool inTimeMachine_Level;
+
+    public enum TimeSections
+    {
+        Menu,
+        Ingame
+    }
+
+    public TimeSections timeStuff;
     // Start is called before the first frame update
     void Start()
     {
-        mainHub.SetActive(true);
-        sceneHub.SetActive(false);
+        if (timeStuff == TimeSections.Menu)
+        {
+            mainHub.SetActive(true);
+            sceneHub.SetActive(false);
+        }
+
+        if (timeStuff == TimeSections.Ingame)
+        {
+            middleHub.SetActive(false);
+        }
+        inTimeMachine_Level = false;
+    }
+
+    private void Update()
+    {
+        TimeStop();
     }
 
     //Scene Selection Codes:=========================================
@@ -26,6 +51,7 @@ public class TimeMachineHub : MonoBehaviour
         mainHub.SetActive(false);
         sceneHub.SetActive(true);
     }
+
 
     public void GoToTutorial()
     {
@@ -47,5 +73,38 @@ public class TimeMachineHub : MonoBehaviour
     public void BacktoMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void MidTimeMachine_UnPause()
+    {
+        if (timeStuff == TimeSections.Ingame)
+        {
+            inTimeMachine_Level = false;
+        }
+    }    
+
+    public void TimeStop()
+    {
+        if (timeStuff == TimeSections.Ingame)
+        {
+            if (inTimeMachine_Level)
+            {
+                Time.timeScale = 0.0f;
+                middleHub.SetActive(true);
+            }    
+            else
+            {
+                Time.timeScale = 1.0f;
+                middleHub.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inTimeMachine_Level = true;
+        }
     }
 }
