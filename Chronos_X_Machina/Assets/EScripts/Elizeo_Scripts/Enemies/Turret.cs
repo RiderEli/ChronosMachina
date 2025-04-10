a3a60e99dafa9ba1a496dfded32b71573cd1d235
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2025-04-09 23:45:06 -0700

    Finished Editing Level 0

--- Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/Enemies/Turret.cs
+++ Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/Enemies/Turret.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Turret : EnemyParent
 {
-    public GameObject poop;
+    //public GameObject poop;
 
     [Header("Is the turret aiming?")]
     public bool aiming;
@@ -39,7 +39,7 @@ public class Turret : EnemyParent
         if (aiming)
         {
             enemyHead.transform.LookAt(player.transform.position);
-            poop.transform.position = player.transform.position;
+           // poop.transform.position = player.transform.position;
             missileShoot();
 
         }
