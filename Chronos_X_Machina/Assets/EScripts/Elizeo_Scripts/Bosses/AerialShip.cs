2ba6820d1330f0ee25283958111a79d3941c2794
Elizeo Nava <116670069+RiderEli@users.noreply.github.com>
2025-05-07 23:29:12 -0700

    PlaneBossProgress - 4

--- Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/Bosses/AerialShip.cs
+++ Chronos_X_Machina/Assets/EScripts/Elizeo_Scripts/Bosses/AerialShip.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static ArmoredTank;
+using UnityEngine.UIElements;
+//using static ArmoredTank;
 
 public class AerialShip : BossParent
 {
@@ -19,11 +21,14 @@ public class AerialShip : BossParent
     public GameObject[] enemies;
 
     //Transformation Stuff
-    //public Transform deploySpawn;
-    //public Transform bombSpawn;
-    public Transform[] thingsToRotateAround;
+    public Transform deploySpawn;
+    public Transform bombSpawn;
 
-    public Transform rotationLookPoint;
+    public Transform[] weaponSpawn_2;
+    public Transform[] thingsToRotateAround;
+    public GameObject[] rotationObjects;
+
+   // public Transform rotationLookPoint;
 
     private int maxPosValue = 5;
     private int minPosValue = 1;
@@ -37,6 +42,7 @@ public class AerialShip : BossParent
     private int maxDepNum = 3;
     [SerializeField] private int planeDeployNum;
 
+    public PlayerHP bossHealth_UI_On_Screen;
     public enum ShipPatterns
     {
         BOMBS,
@@ -64,15 +70,23 @@ public class AerialShip : BossParent
     private bool bossAttacking;
 
     public PlanePhases phases;
-    
+
     // Start is called before the first frame update
     public override void Start()
     {
+        player = GameObject.FindGameObjectWithTag("PlayerTarget");
         bossRB = GetComponent<Rigidbody>();
         currentPosTime = posValueTime;
         deployCounter = deployDelay;
-        posNum = Random.Range(minPosValue, maxPosValue);
-        //planeDeployNum = Random.Range(minDepNum, maxDepNum);
+        posNum = UnityEngine.Random.Range(minPosValue, maxPosValue);
+        planeDeployNum = UnityEngine.Random.Range(minDepNum, maxDepNum);
+
+        //Renderer for the Ship
+        bossRenderer[0] = bossPieces[0].GetComponent<Renderer>();
+        bossRenderer[0].material = bossMat[0];
+
+        //For UI
+        bossHealthActive = false;
     }
 
     // Update is called once per frame
@@ -88,6 +102,27 @@ public class AerialShip : BossParent
         Debug.Log("Deploy Number: " + planeDeployNum);
         //FOR DEBUGGINH PURPOSES
         //PlaneLookDebug();
+
+
+        //General Stuff for UI and Materials
+        if (bossHP <= 0)
+        {
+            Debug.Log("Boss Died, lol");
+            Destroy(this.gameObject);
+            if (WaveChecker.insideWave == true)
+            {
+                WaveSystem.counter -= 1;
+            }
+        }
+
+        if (bossHealthActive)
+        {
+            bossHealth_UI.SetActive(true);
+        }
+        else
+        {
+            bossHealth_UI.SetActive(false);
+        }
     }
 
     //THIS IS ONLY FOR DEBUGGINH
@@ -199,7 +234,7 @@ public class AerialShip : BossParent
 
         if (currentPosTime < 0)
         {
-            posNum = Random.Range(minPosValue, maxPosValue);
+            posNum = UnityEngine.Random.Range(minPosValue, maxPosValue);
 
             planeMovement = PlaneMovements.Straight;
 
@@ -221,64 +256,162 @@ public class AerialShip : BossParent
         {
             if (deployables == ShipPatterns.BOMBS)
             {
-                Instantiate(bossWeapon[1], weaponSpawn[0].transform.position, weaponSpawn[0].transform.rotation);
+                Instantiate(bossWeapon[1], bombSpawn.transform.position, bombSpawn.transform.rotation);
             }
 
             if (deployables == ShipPatterns.MISSILES)
             {
+                //For the Left Cannons
                 Instantiate(bossWeapon[0], weaponSpawn[0].transform.position, weaponSpawn[0].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[1].transform.position, weaponSpawn[1].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[2].transform.position, weaponSpawn[2].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[3].transform.position, weaponSpawn[3].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[4].transform.position, weaponSpawn[4].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[5].transform.position, weaponSpawn[5].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[6].transform.position, weaponSpawn[6].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn[7].transform.position, weaponSpawn[7].transform.rotation);
+
+                //For the Right Cannons
+                Instantiate(bossWeapon[0], weaponSpawn_2[0].transform.position, weaponSpawn_2[0].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[1].transform.position, weaponSpawn_2[1].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[2].transform.position, weaponSpawn_2[2].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[3].transform.position, weaponSpawn_2[3].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[4].transform.position, weaponSpawn_2[4].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[5].transform.position, weaponSpawn_2[5].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[6].transform.position, weaponSpawn_2[6].transform.rotation);
+                Instantiate(bossWeapon[0], weaponSpawn_2[7].transform.position, weaponSpawn_2[7].transform.rotation);
             }
 
             if (deployables == ShipPatterns.GRUNTS)
             {
-                Instantiate(enemies[Random.Range(0, 2)], weaponSpawn[1].transform.position, weaponSpawn[1].transform.rotation);
+                Instantiate(enemies[UnityEngine.Random.Range(0, 2)], deploySpawn.transform.position, deploySpawn.transform.rotation);
             }
 
             deployCounter = deployDelay;
 
-            planeDeployNum = Random.Range(minDepNum, maxDepNum);
+            planeDeployNum = UnityEngine.Random.Range(minDepNum, maxDepNum);
         }
 
 
     }
-        public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (planeMovement == PlaneMovements.Straight)
         {
-            if (other.gameObject.CompareTag("Wave Camera")) //CURRENT PLACEHOLDER FOR ROTATION POINT TAGS
+            if (posNum == 1)
             {
-                planeMovement = PlaneMovements.Rotating;
+                if (other.gameObject == rotationObjects[0])
+                {
+                    planeMovement = PlaneMovements.Rotating;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
             }
-        }
 
+            if (posNum == 2)
+            {
+                if (other.gameObject == rotationObjects[1])
+                {
+                    planeMovement = PlaneMovements.Rotating;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+            }
+
+            if (posNum == 3)
+            {
+                if (other.gameObject == rotationObjects[2])
+                {
+                    planeMovement = PlaneMovements.Rotating;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+            }
+
+            if (posNum == 4)
+            {
+                if (other.gameObject == rotationObjects[3])
+                {
+                    planeMovement = PlaneMovements.Rotating;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+
+                if (posNum == 5)
+                {
+                    if (other.gameObject == rotationObjects[4])
+                    {
+                        planeMovement = PlaneMovements.Rotating;
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                    }
+                }
+
+
+            }
+
+            if (other.gameObject.CompareTag("PlayerWep"))
+            {
+                if (other.gameObject.GetComponent<BulletProjectile>() != null)
+                {
+                    bossHP -= other.gameObject.GetComponent<BulletProjectile>().impactDamage;
+                }
+                else if (other.gameObject.GetComponent<FireProjectile>() != null)
+                {
+                    bossHP -= other.gameObject.GetComponent<FireProjectile>().impactDamage;
+                }
+                else
+                {
+                    bossHP -= 25;
+                }
+                bossHealth_UI_On_Screen.SetHP(bossHP);
+                Destroy(other.gameObject);
+                StartCoroutine(BossGotHit());
+            }
+
+        }
     }
+
+        public IEnumerator BossGotHit()
+        {
+            bossRenderer[0].material = bossMat[1];
+
+            yield return new WaitForSeconds(0.1f);
+            bossRenderer[0].material = bossMat[0];
+
+        }
 
         public void BossDetectBoolThing()
-    {
-        if (bossAttacking == true)
         {
-            PlaneAttacking();
-            Debug.Log("Boss is Attacking");
-        }
-        else if(bossAttacking == false)
-        {
-            Debug.Log("Boss is NOT Attacking");
-        }
+            if (bossAttacking == true)
+            {
+                PlaneAttacking();
+                Debug.Log("Boss is Attacking");
+            }
+            else if (bossAttacking == false)
+            {
+                Debug.Log("Boss is NOT Attacking");
+            }
 
-    }
+        }
 
         public void PlaneDetection()
         {
+            //THIS IS FOR HOW FAR THE BOSS WILL ATTACK
             float distance = Vector3.Distance(transform.position, attackRange.position);
 
-             if (distance < attackDetect)
-             {
-            bossAttacking = true;     
-                   
-             }
-             else
-        {
-            bossAttacking = false;
+            if (distance < attackDetect)
+            {
+                bossAttacking = true;
+
+            }
+            else
+            {
+                bossAttacking = false;
+            }
+
+            //THIS IS MEANT FOR TRIGGERING THE BOSS HP UI
+            float playerDistance = Vector3.Distance(transform.position, player.transform.position);
+
+            if (playerDistance < bossDetect)
+            {
+                bossHealthActive = true;
+            }
         }
     }
-}
+
